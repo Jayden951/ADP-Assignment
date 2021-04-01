0b6b57402531aabb9df83e14d76278d0a4ee6936
luvolwethu02 <80500382+luvolwethu02@users.noreply.github.com>
2021-04-02 01:20:27 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp.assignment;
+
+/**
+ *
+ * @author 219126224 Luvolwethu Anezwa 
+ */
+public class Account {
+
+   private String surname; 
+
+
+  public String getSurname() {
+    return surname;
+     }
+
+ 
+  public void setSurname(String Surname) {
+    this.surname = Surname;
+    }
+  }
+    
