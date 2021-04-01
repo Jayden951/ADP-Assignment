5b5c3aa9565b88ef216be9e1b6ab716a5fe1c76d
Jayden <219086796@my.cpu.ac.za>
2021-04-01 23:13:28 +0200

    Second commit

---
+++ src/main/java/za/ac/cput/adp/assignment/Account.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp.assignment;
+
+/**
+ *
+ * @author Julian
+ */
+public class Account {
+    private String id, name, accountType;
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+      public String getName(){
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+      public String getAccountType(){
+        return accountType;
+    }
+    
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+    
+    @Override
+    public String toString(){
+        return "Account{" + "id=" + id + "name=" + name + "accountType=" + accountType + '}';
+    }
+}
---
+++ src/test/java/za/ac/cput/adp/assignment/AccountTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp.assignment;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ *
+ * @author Julian
+ */
+public class AccountTest {
+    
+    private Account account1;
+    private Account account2;
+    private Account account3;
+    
+    public AccountTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    @Timeout(5)
+    public void setUp() {
+        account1 = new Account();
+        account2 = new Account();
+        account3 = account1;
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+    @Test
+    void testIdentity () {
+        assertSame(account1, account3);
+    }
+   
+    @Test
+    void testEquality () {
+        assertEquals(account1, account3);
+    }
+    
+    @Test
+    void testFail () {
+        fail(" This test failed");
+        assertEquals(account1, account3);
+    }
+    
+    @Test
+    @Disabled
+    void testDisable () {
+        assertEquals(account1, account3);
+    }
+    
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void testTimeout () {
+        assertEquals(account1, account3);
+    }
+}
+
